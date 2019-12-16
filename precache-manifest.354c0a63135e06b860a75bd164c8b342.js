541ff2264fb8cc99c8b777fcb171cb515a43a26b
KruczyM <kruczus5@gmail.com>
2019-12-16 11:43:10 +0100

    Updates

---
+++ precache-manifest.354c0a63135e06b860a75bd164c8b342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b4a9c071531aa8f270b00121ffd320",
+    "url": "/KruczyM/Learning-React/index.html"
+  },
+  {
+    "revision": "1a9f48df0ce8b74b1e1a",
+    "url": "/KruczyM/Learning-React/static/css/main.f28c4d24.chunk.css"
+  },
+  {
+    "revision": "f379b4aec9f6d3993399",
+    "url": "/KruczyM/Learning-React/static/js/2.0a13fc44.chunk.js"
+  },
+  {
+    "revision": "da01034c70d479d890a70d42d48c6305",
+    "url": "/KruczyM/Learning-React/static/js/2.0a13fc44.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a9f48df0ce8b74b1e1a",
+    "url": "/KruczyM/Learning-React/static/js/main.9d891c72.chunk.js"
+  },
+  {
+    "revision": "4379be66be6baa81cb44",
+    "url": "/KruczyM/Learning-React/static/js/runtime-main.e9aadb23.js"
+  }
+]);
